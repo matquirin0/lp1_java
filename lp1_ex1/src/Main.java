7b9db07f12cfaa573eaa9e31f429ac8d5f5090bb
Matheus Quirino <contato.matheuspquirino@gmail.com>
2026-02-27 12:17:43 -0300

    feat (ex1/ex2/ex3) : Exercícios 1, 2 e 3.

---
+++ lp1_ex1/src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //Exercício 1 e 2
+        System.out.println("Linguagem de Programação 1");
+        System.out.println("Aluno: Matheus Quirino");
+        System.out.println("Prof.: Adriana Jacinto");
+
+        System.out.println("==========================");
+
+        //Exercício 3
+        System.out.print("Linguagem de Programação 1\n");
+        System.out.print("Aluno: Matheus Quirino\n");
+        System.out.print("Prof.: Adriana Jacinto\n");
+
+        }
+    }
